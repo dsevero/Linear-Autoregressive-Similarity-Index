470b9b20d6686b2cf680e26f7c87f86314e9656a
shreshthsaini <shreshthfeel@gmail.com>
2024-01-26 12:02:54 -0600

    updating jax install version to make sure it works for wider options.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip install --upgrade pip
-pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install -e .
